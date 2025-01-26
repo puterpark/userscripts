5cdd08f779d3fe3ee833cd4e284a1d5aca9f503b
Puter <36284842+puterpark@users.noreply.github.com>
2025-01-26 13:31:04 +0900

    add: trickcal_coupon.js

---
+++ trickcal_coupon.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         트릭컬 리바이브 쿠폰 회원번호 설정
+// @namespace    https://puter.us
+// @version      2025-01-26
+// @description  회원번호를 입력해 줍니다.
+// @author       Puter
+// @match        https://coupon.a.prod.service.trickcal.io
+// @icon         https://web-cdn.aws.trickcal.io/web/TrickcalRevive_Favicon.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.getElementById('UserId').value = '1635693'
+})();
